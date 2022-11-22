3cca14aa23999df6fd7502972bd518b25ce66740
juananton <juananton.correo@gmail.com>
2022-11-22 20:00:35 +0100

    Improve Button component

--- src/components/forms/Button.jsx
+++ src/components/forms/Button.jsx
@@ -1,8 +1,26 @@
 import style from './Button.module.css';
 
-const Button = ({ className, children }) => {
+const BTN_TYPES = {
+	text: style.text,
+	icon: style.icon
+};
+const BTN_USES = {
+	primary: style.primary,
+	secondary: style.secondary,
+	disabled: style.disabled
+};
+
+const Button = ({ type = 'text', use = 'secondary', ...props }) => {
+	const btnType = BTN_TYPES[type];
+	const btnUse = BTN_USES[use];
+
 	return (
-		<button className={`${style.button} ${className || ''}`}>{children}</button>
+		<button
+			onClick={props.onClick}
+			className={`${style.button} ${btnType} ${btnUse}`}
+		>
+			{props.children}
+		</button>
 	);
 };
 

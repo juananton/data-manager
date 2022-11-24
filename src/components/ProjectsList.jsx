07a12804070b5e53fa8131d3e8a88751de231ec4
juananton <juananton.correo@gmail.com>
2022-11-24 07:35:52 +0100

    Create empty state

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -1,12 +1,18 @@
+import style from './ProjectsList.module.css';
 import ProjectsListItem from './ProjectsListItem';
 
 const ProjectsList = ({ projects, itemsPerPage }) => {
 	const listItemHeight = 56; // Try to get it dinamically
+
 	return (
 		<div style={{ height: itemsPerPage * listItemHeight }}>
-			{projects.map(project => (
-				<ProjectsListItem key={project.id} {...project} />
-			))}
+			{projects.length === 0 ? (
+				<p className={style.emptyState}>No projects to display</p>
+			) : (
+				projects.map(project => (
+					<ProjectsListItem key={project.id} {...project} />
+				))
+			)}
 		</div>
 	);
 };
